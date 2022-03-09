d6c8bb76b4fb7356523ec67f6198464e3d0097dd
Eli Kosminsky <eli@tangibleplay.com>
2022-03-09 12:12:49 -0800

    initial commit

---
+++ Scripts/BloomBoxController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloomBoxController : MonoBehaviour {
+
+    [SerializeField] List<BlossomController> blossoms = new List<BlossomController>();
+
+    [SerializeField] Text onesLabel;
+    [SerializeField] Text tensLabel;
+    [SerializeField] Text hundredsLabel;
+    [SerializeField] Text sumLabel;
+
+    public void StartMachine() {
+        sumLabel.text = "0";
+
+        GoTweenChain tweenChain = new GoTweenChain();
+
+        List<BlossomController> activeBlossoms = blossoms.Where(blossom => blossom.GetBucket() > 0).OrderBy(blossom => blossom.GetBucket()).ToList();
+
+        GoTweenFlow makeFlow = new GoTweenFlow();
+        activeBlossoms.ForEach(blossom => makeFlow.insert(0, blossom.MakeFlowerPower()));
+        tweenChain.append(makeFlow);
+
+        GoTweenFlow moveFlow = new GoTweenFlow();
+        for (int i = 0; i < activeBlossoms.Count; i++) {
+            BlossomController blossom = activeBlossoms[i];
+            AbstractGoTween sendTween = blossom.SendFlowerPower(sumLabel.gameObject.transform);
+            moveFlow.insert(i * 0.5f, sendTween);
+            sendTween.setOnCompleteHandler(agt => sumLabel.text = (int.Parse(sumLabel.text) + blossom.GetBucket()).ToString());
+        }
+        tweenChain.append(moveFlow);
+
+        tweenChain.play();
+    }
+
+    private void Start() {
+        sumLabel.text = "0";
+    }
+
+    private void Update() {
+        int onesSum = blossoms.Count(blossom => blossom.GetBucket() == 1);
+        int tensSum = blossoms.Count(blossom => blossom.GetBucket() == 10) * 10;
+        int hundredsSum = blossoms.Count(blossom => blossom.GetBucket() == 100) * 100;
+
+        onesLabel.text = onesSum.ToString();
+        tensLabel.text = tensSum.ToString();
+        hundredsLabel.text = hundredsSum.ToString();
+    }
+}
---
+++ Scripts/BlossomController.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BlossomController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    [SerializeField] public RectTransform ones;
+    [SerializeField] public RectTransform tens;
+    [SerializeField] public RectTransform hundreds;
+
+    [SerializeField] public GameObject flowerPowerPrefab;
+    [SerializeField] public GameObject flowerPowerParent;
+
+    private bool dragging = false;
+    private int bucket = 0;
+    private List<GameObject> flowerPowers = new List<GameObject>();
+
+    public int GetBucket() {
+        return bucket;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        dragging = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        dragging = false;
+    }
+
+    public AbstractGoTween MakeFlowerPower() {
+        if (bucket == 100) {
+            GoTweenFlow makeFlow = new GoTweenFlow();
+
+            for (int i = 0; i < bucket; i++) {
+                float radiansPerOrb = (2 * Mathf.PI) / 20;
+                float distPerOrb = 2;
+
+                float angle = radiansPerOrb * i;
+
+                GameObject flowerPower = Instantiate(flowerPowerPrefab, gameObject.transform);
+                flowerPower.transform.localScale = Vector3.zero;
+                flowerPower.transform.localPosition = Vector3.zero;
+                flowerPower.transform.SetParent(flowerPowerParent.transform, true);
+                flowerPowers.Add(flowerPower);
+
+                float dist = distPerOrb * i;
+                makeFlow.insert(0, Go.to(flowerPower.transform, 1, new GoTweenConfig()
+                    .scale(1)
+                    .position(new Vector3(dist * Mathf.Cos(angle), dist * Mathf.Sin(angle)), true)
+                    .setEaseType(GoEaseType.BackOut))
+                );
+            }
+
+            return makeFlow;
+        } else if (bucket == 10) {
+            GoTweenFlow makeFlow = new GoTweenFlow();
+
+            for (int i = 0; i < bucket; i++) {
+                float angle = (2 * Mathf.PI * i) / bucket;
+
+                GameObject flowerPower = Instantiate(flowerPowerPrefab, gameObject.transform);
+                flowerPower.transform.localScale = Vector3.zero;
+                flowerPower.transform.localPosition = Vector3.zero;
+                flowerPower.transform.SetParent(flowerPowerParent.transform, true);
+                flowerPowers.Add(flowerPower);
+
+                makeFlow.insert(0, Go.to(flowerPower.transform, 1, new GoTweenConfig()
+                    .scale(1)
+                    .position(new Vector3(100 * Mathf.Cos(angle), 100 * Mathf.Sin(angle)), true)
+                    .setEaseType(GoEaseType.BackOut))
+                );
+            }
+
+            return makeFlow;
+        } else {
+            GameObject flowerPower = Instantiate(flowerPowerPrefab, gameObject.transform);
+            flowerPower.transform.localScale = Vector3.zero;
+            flowerPower.transform.localPosition = Vector3.zero;
+            flowerPower.transform.SetParent(flowerPowerParent.transform, true);
+            flowerPowers.Add(flowerPower);
+
+            return Go.to(flowerPower.transform, 1, new GoTweenConfig()
+                .scale(1)
+                .setEaseType(GoEaseType.BackOut)
+            );
+        }
+    }
+
+    public AbstractGoTween SendFlowerPower(Transform target) {
+        GoTweenFlow sendFlow = new GoTweenFlow();
+
+        for (int i = 0; i < flowerPowers.Count; i++) {
+            GameObject flowerPower = flowerPowers[i];
+
+            GoTweenChain chain = new GoTweenChain();
+
+            //chain.append(Go.to(flowerPower.transform, 1, new GoTweenConfig()
+            //    .position(new Vector3(0, 100), true)
+            //    .setEaseType(GoEaseType.CubicOut)
+            //));
+
+            chain.append(Go.to(flowerPower.transform, 1.25f, new GoTweenConfig()
+                .position(target.position)
+                .sizeDelta(Vector2.zero)
+                .setEaseType(GoEaseType.BackIn)
+            ));
+
+            sendFlow.insert(0, chain);
+        }
+
+        return sendFlow;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (dragging) {
+            transform.position = FlattenPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            if (BucketContainsMouse(ones)) {
+                bucket = 1;
+            } else if (BucketContainsMouse(tens)) {
+                bucket = 10;
+            } else if (BucketContainsMouse(hundreds)) {
+                bucket = 100;
+            } else {
+                bucket = 0;
+            }
+        }
+
+        Vector2 relativePoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(ones, Input.mousePosition, Camera.main, out relativePoint);
+    }
+
+    private Vector3 FlattenPoint(Vector3 point) {
+        return new Vector3(point.x, point.y, 0);
+    }
+
+    private bool BucketContainsMouse(RectTransform bucket) {
+        Vector2 relativePoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(bucket, Input.mousePosition, Camera.main, out relativePoint);
+        return bucket.rect.Contains(relativePoint);
+    }
+
+}
